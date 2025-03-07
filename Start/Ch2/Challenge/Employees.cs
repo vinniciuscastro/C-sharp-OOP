d5d2390ba626365d93bbc3aca68278592c7a2ceb
Vinnicius Castro <90991299+vinniciuscastro@users.noreply.github.com>
2025-03-07 09:17:38 -0700

    2

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -2,11 +2,33 @@
 // Solution to the Employee Class challenge
 
 public class Employee {
+    
     public Employee() {}
+    
+    public required int ID {get; set;}
+    public required string? Department {get; set;}
+    public required string? FullName {get; set;}
 
-    // YOUR CODE GOES HERE
+    public void AdjustPay(decimal percent) {}
+       // YOUR CODE GOES HERE
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
+}
+
+public class HourlyEmployee : Employee {
+    public required decimal PayRate {get; set;}
+    public void AdjustPay(decimal percent) {
+        PayRate += PayRate * percent;
+    }
+       // YOUR CODE GOES HERE
+}
+
+public class SalariedEmployee : Employee {
+    public required decimal Salary {get; set;}
+    public void AdjustPay(decimal percent) {
+        Salary += Salary * percent;
+    }
+       // YOUR CODE GOES HERE
 }
 
 // DEFINE OTHER CLASSES HERE
